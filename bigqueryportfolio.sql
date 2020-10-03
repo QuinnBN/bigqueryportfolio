c0177b280b01a128fc6fc033417aa79d5c39b901
Budiono Santoso <budiono1995@gmail.com>
2020-10-04 01:01:17 +0700

    Create bigqueryportfolio.sql

---
+++ bigqueryportfolio.sql
@@ -0,0 +1,7 @@
+SELECT Store_Number, Store_Name, Ownership_Type, Street_Address, City
+FROM `budiono-2020.starbucks.starbucks`
+WHERE Country = "ID" 
+
+SELECT Store_Number, Store_Name, Ownership_Type, Street_Address, City
+FROM `budiono-2020.starbucks.starbucks`
+WHERE Country = "ID" AND City = "Jakarta"
