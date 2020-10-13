94687b3c53116fcd36dfaab9167f0b66d4dc3003
Budiono Santoso <budiono1995@gmail.com>
2020-10-13 09:56:09 +0700

    Update bigqueryportfolio.sql

--- bigqueryportfolio.sql
+++ bigqueryportfolio.sql
@@ -2,6 +2,18 @@ SELECT Store_Number, Store_Name, Ownership_Type, Street_Address, City
 FROM `budiono-2020.starbucks.starbucks`
 WHERE Country = "ID" 
 
+#Row	Store_Number	Store_Name	Ownership_Type	Street_Address	City
+1 35627-96421 Bali Discovery Mall Licensed JL Kartika Plaza, Ground Floor Bali
+2 31921-116544 Highway 5 KM 13 Rest Area Licensed Rest Area KM 13,5, Tangerang Banten
+3 31922-118770 Gading Serpong Mall Licensed Blvd Gading Serpong Blok AA-4 No 43-45, Ground Floor # 36 Tangerang
+
 SELECT Store_Number, Store_Name, Ownership_Type, Street_Address, City
 FROM `budiono-2020.starbucks.starbucks`
 WHERE Country = "ID" AND City = "Jakarta"
+
+#Row	Store_Number	Store_Name	Ownership_Type	Street_Address	City
+1 18642-193804 Starbucks Gambir Licensed Jl. Medan Merdeka Timur, Area Keberangkatan Jakarta
+2 35615-87284 BNI' 46 Licensed JL Jend Sudirman Kav 1, Ground Floor, External Suite G.17 Jakarta
+3 18970-197408 Central Park 3 Licensed Jl. S Parman Kav 28, L1-133, First Floor Jakarta
+4 17446-182010 Menara Thamrin Licensed Jl. M H Thamrin Kav 3, Ground Floor Jakarta
+
